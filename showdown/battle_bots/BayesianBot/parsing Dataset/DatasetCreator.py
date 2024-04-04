03864ce703151723e192c69058bf180d31cefff2
TechRufy <alb.genovese@gmail.com>
2024-04-04 19:56:28 +0200

    secondo prototipo parser, aggiunto meteo e vita pokemon

--- showdown/battle_bots/BayesianBot/parsing Dataset/DatasetCreator.py
+++ showdown/battle_bots/BayesianBot/parsing Dataset/DatasetCreator.py
@@ -1,9 +1,22 @@
 from Parser import parser
 import pandas as pd
+import os
 
+df = pd.DataFrame(columns=["User", "Sufferer", "name move", "TypesU",
+                           "TypesS", "TypeM", "power", "UserHP", "SuffererHP"])
+for cartella, sottocartelle, files in os.walk(os.getcwd() + "\\log"):
+    if not files.__contains__("Dataset.csv"):
+        df.to_csv(".\\log\\Dataset.csv",
+                  columns=["User", "Sufferer", "name move", "TypesU",
+                           "TypesS", "TypeM", "power", "UserHP", "SuffererHP"],index=False)
+    for file in files:
+        if file == "Dataset.csv":
+            continue
+        with open(os.getcwd() + "\\log\\" + file, 'r+') as movefile:
+            df = parser(movefile)
+            print(df)
 
-with open("Gen7RandomBattle-2024-03-27-eimint64-shnen.log", 'r+') as movefile:
-    df = parser(movefile)
-
-
-    df.to_csv("Gen7RandomBattle-2024-03-27-eimint64-shnen.csv")
+        dataset = pd.read_csv(os.getcwd() + "\\log\\" + "Dataset.csv")
+        print(dataset)
+        dataset = pd.concat([dataset, df], ignore_index=True)
+        dataset.to_csv(".\\log\\Dataset.csv",index=False)
--- showdown/battle_bots/BayesianBot/parsing Dataset/Parser.py
+++ showdown/battle_bots/BayesianBot/parsing Dataset/Parser.py
@@ -13,27 +13,30 @@ def parser(log_data):
         pokedex = json.load(pokedexfile)
         pokedexfile.close()
 
-    df = pd.DataFrame(columns=["User", "Sufferer", "name move", "TypesU", "TypesS", "TypeM", "power"])
+    df = pd.DataFrame(columns=["User", "Sufferer", "name move", "TypesU",
+                               "TypesS", "TypeM", "power", "UserHP", "SuffererHP", "Weather"])
 
     # Regular expressions for different types of log entries
     player_regex = re.compile(r'\|player\|(\w+)\|(\w+)\|(\d+)\|(\d+)')
-    switch_regex = re.compile(r'\|switch\|(\w+): (\w+)\|(\w+), L(\d+)\|(\d+)\/(\d+)')
+    switch_regex = re.compile(r'\|switch\|p\da: [A-Za-z]+\|(.*?), L[0-9]+[,]?\s?[M,F]?\|[0-9]+\\/[0-9]+')
     move_regex = re.compile(r'\|move\|p\da: [A-Za-z]+\|(.*?)\|p\da: [A-Za-z]+')
-    moveP_regex = re.compile(r'\|move\|p\da: [A-Za-z]+\|[A-Za-z]+\|\|\[[A-Za-z]+\]')
+    moveP_regex = re.compile(r'\|move\|p\da: [A-Za-z]+\|(.*?)\|\|\[[A-Za-z]+\]')
+    weather_regex = re.compile(r'\|-weather\|(.*?)[|]?(.*?)')
 
     # Function to parse the log data
+    mapping_table = str.maketrans({' ': '', '-': ''})
+    pokemon = {"p1a Pokemon": None, "p2a Pokemon": None, "hp p1a": 0, "hp p2a": 0, "weather": "none"}
     for line in log_data:
-        if line.startswith('|player|'):
-            match = player_regex.match(line)
-            if match:
-                side, username, pokemon_id, elo = match.groups()
-                print(f"Player: {side}, Username: {username}, Pokemon ID: {pokemon_id}, ELO: {elo}")
-        elif line.startswith('|switch|'):
+        if line.startswith('|switch|'):
             match = switch_regex.match(line)
-            if match:
-                side, pokemon_name, species, level, hp_current, hp_max = match.groups()
-                print(
-                    f"Switch: {side}, Pokemon: {pokemon_name}, Species: {species}, Level: {level}, HP: {hp_current}/{hp_max}")
+            matches = match.group().split("|")
+            for token in matches:
+                if token.startswith("p1a"):
+                    pokemon["p1a Pokemon"] = token[5:]
+                    pokemon["hp p1a"] = matches[-1]
+                if token.startswith("p2a"):
+                    pokemon["p2a Pokemon"] = token[5:]
+                    pokemon["hp p2a"] = matches[-1]
         elif line.startswith('|move|'):
             match2 = None
             match1 = move_regex.match(line)
@@ -48,33 +51,41 @@ def parser(log_data):
                 for item in matches:
                     if item.startswith("p"):
                         if User is None:
-                            User = item[5:]
+                            User = item
                         else:
-                            Sufferer = item[5:]
+                            Sufferer = item
                     else:
                         Move = item
-                mapping_table = str.maketrans({' ': '', '-': ''})
-                df.loc[len(df)] = [User, Sufferer, Move,
-                                   pokedex[User.lower()]["types"],
-                                   pokedex[Sufferer.lower()]["types"],
+                df.loc[len(df)] = [User[5:], Sufferer[5:], Move,
+                                   pokedex[User[5:].lower()]["types"],
+                                   pokedex[Sufferer[5:].lower()]["types"],
                                    moves[Move.translate(mapping_table).lower()]["type"],
-                                   moves[Move.translate(mapping_table).lower()]["basePower"]]
+                                   moves[Move.translate(mapping_table).lower()]["basePower"],
+                                   pokemon["hp " + User[:3]],
+                                   pokemon["hp " + Sufferer[:3]],
+                                   pokemon["weather"]
+                                   ]
             elif match2:
                 Move = None
                 User = None
                 for item in matches[:-2]:
                     if item.startswith("p"):
                         if User is None:
-                            User = item[5:]
+                            User = item
                     else:
                         Move = item
-                mapping_table = str.maketrans({' ': '', '-': ''})
-                df.loc[len(df)] = [User, User, Move,
-                                   pokedex[User.lower()]["types"],
-                                   pokedex[User.lower()]["types"],
+                df.loc[len(df)] = [User[5:], User[5:], Move,
+                                   pokedex[User[5:].lower()]["types"],
+                                   pokedex[User[5:].lower()]["types"],
                                    moves[Move.translate(mapping_table).lower()]["type"],
-                                   moves[Move.translate(mapping_table).lower()]["basePower"]]
-
-
+                                   moves[Move.translate(mapping_table).lower()]["basePower"],
+                                   pokemon["hp " + User[:3]],
+                                   pokemon["hp " + User[:3]],
+                                   pokemon["weather"]
+                                   ]
+        elif line.startswith("|-weather|"):
+            matches = line.split("|")
+            print(matches)
+            pokemon["weather"] = matches[2]
 
     return df
